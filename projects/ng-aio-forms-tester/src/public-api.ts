77ea5778aeecadfb5cf0dc1f7af4896fc3edf20e
NOPR9D <boucham-amine@mylabz.xyz>
2022-01-22 19:34:51 +0100

    :egg: : creation

---
+++ projects/ng-aio-forms-tester/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ng-aio-forms-tester
+ */
+
+export * from './lib/ng-aio-forms-tester.service';
+export * from './lib/ng-aio-forms-tester.component';
+export * from './lib/ng-aio-forms-tester.module';
---
+++ projects/ng-aio-forms/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ng-aio-forms
+ */
+
+export * from './lib/ng-aio-forms.service';
+export * from './lib/ng-aio-forms.component';
+export * from './lib/ng-aio-forms.module';
