47d4891cad18b5d61363fcc2bc0fd83d11532f33
NOPR9D <boucham-amine@mylabz.xyz>
2022-02-01 01:21:09 +0100

    :lipstick: : background

--- projects/ng-aio-forms-website/src/app/app.module.ts
+++ projects/ng-aio-forms-website/src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  AboutComponent,
+  BackgroundComponent,
+  ContactComponent,
+  DocsComponent,
+  ExemplesComponent,
+  FooterComponent,
+  HeaderComponent,
+} from './components';
+import { BackgroundProvider } from './providers';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    BackgroundComponent,
+    ContactComponent,
+    DocsComponent,
+    ExemplesComponent,
+    FooterComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [BackgroundProvider],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ projects/ng-aio-forms-website/src/app/components/exemples/exemples.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exemples',
+  templateUrl: './exemples.component.html',
+  styleUrls: ['./exemples.component.scss']
+})
+export class ExemplesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
