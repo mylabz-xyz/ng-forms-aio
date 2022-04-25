153ec950d93de7a42fb4cacb8e5e1573e4055614
noprod <issue@boucham-amine.fr>
2022-04-25 03:35:21 +0200

    :white_check_mark: : checkbox test

--- projects/ng-aio-forms/src/lib/components/checkbox-list/checkbox-list.component.spec.ts
+++ projects/ng-aio-forms/src/lib/components/checkbox-list/checkbox-list.component.spec.ts
@@ -1,24 +1,71 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { BaseAbstractGridSpecs } from 'projects/ng-aio-forms/src/test/index';
+import { NgAioCheckboxFormComponent } from '..';
 
 import { NgAioCheckboxListFormComponent } from './checkbox-list.component';
 
-describe('CheckboxComponent', () => {
-  let component: NgAioCheckboxListFormComponent;
-  let fixture: ComponentFixture<NgAioCheckboxListFormComponent>;
+@Component({
+  selector: 'test-checkbox',
+  template: `<div>
+    <ng-aio-checkbox
+      (onChange)="onChange($event)"
+      [value]="value"
+    ></ng-aio-checkbox>
+  </div>`,
+})
+class InputTextAreaWrapComponent {
+  public lastEvent: any = '';
+  public value = '';
+  public values = [
+    { label: 'foo', value: 'foo' },
+    { label: 'bar', value: 'bar' },
+  ];
+  public onChange(event: { value: string; id: string }) {
+    console.log(event);
+    this.lastEvent = event.value;
+  }
+}
+describe('TextBox Component :', () => {
+  let baseTestComponent =
+    new BaseAbstractGridSpecs<InputTextAreaWrapComponent>();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [NgAioCheckboxListFormComponent],
-    }).compileComponents();
-  });
+  baseTestComponent.init(
+    NgAioCheckboxFormComponent,
+    InputTextAreaWrapComponent
+  );
+  baseTestComponent.baseTest();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NgAioCheckboxListFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  // it('It should emit input on input value change', fakeAsync(() => {
+  //   const input =
+  //     baseTestComponent.fixture.debugElement.nativeElement.querySelector(
+  //       'input'
+  //     );
+  //   const spyChildEmitEvent = spyOn(
+  //     baseTestComponent.child as NgAioCheckboxFormComponent,
+  //     'emit'
+  //   ).and.callThrough();
+  //   const spyParentOnSubmit = spyOn(
+  //     baseTestComponent.component,
+  //     'onChange'
+  //   ).and.callThrough();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  //   input.click();
+
+  //   expect(spyChildEmitEvent).toHaveBeenCalled();
+  //   expect(spyParentOnSubmit).toHaveBeenCalled();
+  //   expect(baseTestComponent.component.lastEvent).toBe(true);
+  // }));
+
+  // it('It should get an intial value', () => {
+  //   baseTestComponent.component.value = 'foo';
+
+  //   baseTestComponent.fixture.detectChanges();
+
+  //   const input =
+  //     baseTestComponent.fixture.debugElement.nativeElement.querySelector(
+  //       'input'
+  //     );
+  //   expect(input.value).toBe('foo');
+  // });
 });
--- projects/ng-aio-forms/src/lib/components/checkbox/checkbox.component.spec.ts
+++ projects/ng-aio-forms/src/lib/components/checkbox/checkbox.component.spec.ts
@@ -1,24 +1,66 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { BaseAbstractGridSpecs } from 'projects/ng-aio-forms/src/test/index';
+import { NgAioCheckboxFormComponent } from '..';
 
-import { NgAioCheckboxFormComponent } from './checkbox.component';
+@Component({
+  selector: 'test-checkbox',
+  template: `<div>
+    <ng-aio-checkbox
+      (onChange)="onChange($event)"
+      [value]="value"
+      [values]="values"
+    ></ng-aio-checkbox>
+  </div>`,
+})
+class CheckBoxWrapComponent {
+  public lastEvent: any = '';
+  public value = '';
+  public values = [
+    { label: 'foo', value: 'foo' },
+    { label: 'bar', value: 'bar' },
+  ];
+  public onChange(event: { value: string; id: string }) {
+    console.log(event);
+    this.lastEvent = event.value;
+  }
+}
+describe('TextBox Component :', () => {
+  let baseTestComponent = new BaseAbstractGridSpecs<CheckBoxWrapComponent>();
 
-describe('CheckboxComponent', () => {
-  let component: NgAioCheckboxFormComponent;
-  let fixture: ComponentFixture<NgAioCheckboxFormComponent>;
+  baseTestComponent.init(NgAioCheckboxFormComponent, CheckBoxWrapComponent);
+  baseTestComponent.baseTest();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [NgAioCheckboxFormComponent],
-    }).compileComponents();
-  });
+  it('It should emit foo on checkbox checked change', fakeAsync(() => {
+    const input =
+      baseTestComponent.fixture.debugElement.nativeElement.querySelector(
+        'input'
+      );
+    const spyChildEmitEvent = spyOn(
+      baseTestComponent.child as NgAioCheckboxFormComponent,
+      'emit'
+    ).and.callThrough();
+    const spyParentOnSubmit = spyOn(
+      baseTestComponent.component,
+      'onChange'
+    ).and.callThrough();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NgAioCheckboxFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    input.click();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spyChildEmitEvent).toHaveBeenCalled();
+    expect(spyParentOnSubmit).toHaveBeenCalled();
+    expect(baseTestComponent.component.lastEvent).toBe('foo');
+  }));
+
+  it('It should get bar for initial value', () => {
+    baseTestComponent.component.value = 'bar';
+
+    baseTestComponent.fixture.detectChanges();
+
+    const input =
+      baseTestComponent.fixture.debugElement.nativeElement.querySelector(
+        'input'
+      );
+    expect(input.value).toBe('foo');
   });
 });
